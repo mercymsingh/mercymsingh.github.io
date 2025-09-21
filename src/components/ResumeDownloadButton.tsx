1ff5a2cb1bfe358255f0c1fdcbf1dba8440006a1
mercy <mercymochary4@gmail.com>
2025-09-21 20:06:47 +0530

    button style

--- src/components/ResumeDownloadButton.tsx
+++ src/components/ResumeDownloadButton.tsx
@@ -41,8 +41,15 @@ const ResumeDownloadButton: React.FC<ResumeDownloadButtonProps> = ({ data }) => 
       fileName="Mercy_Mochary_Lead_Information_Developer_Resume.pdf"
     >
       {({ loading }) => (
-        <Button className="bg-slate-100 text-slate-900 hover:bg-slate-300">
-          {loading ? 'Loading...' : 'Download'}
+        <Button className="bg-gray-800 text-white font-bold py-2 px-4 rounded-full inline-flex items-center hover:bg-gray-700 transition-colors">
+          {loading ? 'Loading...' : (
+                <>
+                    <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                    </svg>
+                    <span>Download Resume</span>
+                </>
+            )}
         </Button>
       )}
     </PDFDownloadLink>
